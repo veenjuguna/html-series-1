89164fb5a16f70170b89e196ae3ee18f6c3a02aa
veenjuguna <167479023+veenjuguna@users.noreply.github.com>
2025-01-13 21:23:38 +0300

    hj

--- home.py
+++ home.py
@@ -2,6 +2,6 @@ def greet_user(name):
     return f"Hello, {name}! Welcome to the Python world."
 
 if __name__ == "__main__":
-    user_name = input("Enter your name please: ")
+    user_name = input("Enter your name pleaseeeeeeeee: ")
     greeting = greet_user(user_name)
     print(greeting)
