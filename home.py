c9c6fc3edca7d27b1e269e0712bb40ed1c73a7af
veenjuguna <167479023+veenjuguna@users.noreply.github.com>
2025-01-21 21:07:02 +0300

    dfg

--- home.py
+++
@@ -1,7 +0,0 @@
-def greet_user(name):
-    return f"Hello, {name}! Welcome to the Python world."
-
-if __name__ == "__main__":
-    user_name = input("Enter your address ")
-    greeting = greet_user(user_name)
-    print(greeting)
