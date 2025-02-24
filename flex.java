204e5a4c8ee1200a0bd53926c06ddacd5caf045a
veenjuguna <167479023+veenjuguna@users.noreply.github.com>
2025-02-24 22:52:19 +0300

    hello

--- flex.java
+++ flex.java
@@ -1,9 +1,6 @@
 import java.util.Random;
-import java.util.Scanner;
-import java.util.Arrays;
-import java.util.ArrayList;
-import java.util.Collections;
-public class Flex {
+
+public class flex {
     public static void main(String[] args) {
         Random random = new Random();
         
